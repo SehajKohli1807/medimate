6c8cc417db860783bce3894abb83d21638e70bf9
SehajKohli1807 <kohlisehaj18@gmail.com>
2024-06-01 18:20:17 +0530

    01-Layout

---
+++ Components/Footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer footer-center p-4 bg-base-300 text-base-content">
+      <p>All rights reserved by Medimate</p>
+    </div>
+  );
+};
+
+export default Footer;
